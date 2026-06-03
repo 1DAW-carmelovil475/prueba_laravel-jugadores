4613eb1028b06b71b7ef31db62788fc41e0a049b
1DAW-carmelovil475 <cvilpiz939@g.educaand.es>
2026-06-03 14:47:03 +0200

    Proyecto Laravel Completo

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
 use App\Http\Controllers\JugadorController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('jugadores', JugadorController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('jugadores', JugadorController::class)->middleware('auth');
+
+require __DIR__.'/auth.php';
